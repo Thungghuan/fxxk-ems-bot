84ed5a28b281c755b8967f29dc2cf7149cd13bd5
Thungghuan <1092887426@qq.com>
2021-07-25 19:27:02 +0800

    revert: not use webhook for now

--- index.ts
+++ index.ts
@@ -7,10 +7,10 @@ const bot = new Telegraf(token)
 bot.start((ctx) => ctx.reply('Hello'))
 bot.help((ctx) => ctx.reply('Help message'))
 bot.command('hello', (ctx) => ctx.reply('Hello'))
-// bot.launch()
+bot.launch()
 
-bot.telegram.setWebhook(`https://fxxk-ems-bot.herokuapp.com/bot${token}`);
-bot.startWebhook(`/bot${token}`, null, 8000)
+// bot.telegram.setWebhook(`https://fxxk-ems-bot.herokuapp.com/bot${token}`)
+// bot.startWebhook(`/bot${token}`, null, 8000)
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
